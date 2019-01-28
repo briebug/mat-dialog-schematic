b492f7f714b466ca3d81205e4ae2b81a7f21de4b
Anthony Jones <anthonyjones0519@gmail.com>
2019-01-28 09:34:22 -0700

    cleaned up sandbox

--- sandbox/src/app/app.module.ts
+++ sandbox/src/app/app.module.ts
@@ -4,9 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
