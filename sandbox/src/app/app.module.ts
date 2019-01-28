2dcf6fed1c71c4bb2830b749568914bd62719be9
Anthony Jones <anthonyjones0519@gmail.com>
2019-01-28 15:45:38 -0700

    cleaned up sandbox

--- sandbox/src/app/app.module.ts
+++ sandbox/src/app/app.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TestNameComponent } from './test-name/test-name.component';
-import { TestNameDialogComponent } from './test-name-dialog/test-name-dialog.component';
-import { MatButtonModule, MatDialogModule } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent, TestNameComponent, TestNameDialogComponent],
-  imports: [BrowserModule, MatButtonModule, MatDialogModule, BrowserAnimationsModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent],
-  entryComponents: [TestNameDialogComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
